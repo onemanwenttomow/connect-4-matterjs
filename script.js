e9ec540e69a62185a15d2ba1166a13636be4f3e0
Peter Anderson <pete.anderson@gmail.com>
2019-12-09 19:32:29 +0100

    rows and cols working (needs more testing)

--- script.js
+++ script.js
@@ -69,6 +69,7 @@ window.addEventListener("load", function() {
 
     var boardPositions = [];
     var position, col, row, gameOver;
+    var count = 0;
 
     function checkPositions() {
         gameOver = false;
@@ -82,6 +83,8 @@ window.addEventListener("load", function() {
             }
         }
         console.log("*********");
+        count ++;
+        console.log("caling: ", count, gameOver);
         if (gameOver) { return; }
         setTimeout(checkPositions, 1000);
     }
@@ -92,17 +95,20 @@ window.addEventListener("load", function() {
     function generateBoardArray() {
         boardPositions = [];
         for (var k = 0; k < 7; k++) {
-            boardPositions.push(new Array);
+            boardPositions.push(["x", "x", "x", "x", "x", "x"]);
         }
+        console.log("boardPositions: ", boardPositions);
     }
 
     function updateBoardPosition(col, row, colour) {
-        if (row !== 0 && !boardPositions[col][row - 1]) { return; }
+        if (row !== 0 && boardPositions[col][row - 1] !== colour[0]) { return; }
         boardPositions[col][row] = colour[0];
         if (colVictory()) {
+            console.log("COL VIC");
             return true;
         }
         if (rowVictory()) {
+            console.log("ROW VIC!");
             return true;
         }
         // console.log("Board Posititons: *****************");
@@ -117,7 +123,7 @@ window.addEventListener("load", function() {
     }
 
     function colVictory(){
-        var domCols = document.getElementsByClassName('col');
+        var domCols = Array.from(document.getElementsByClassName('col'));
         for (var i = 0; i < boardPositions.length; i++) {
             if (boardPositions[i].join("").indexOf("rrrr") > -1) {
                 addWinningClasses(boardPositions[i], "r", domCols[i].children);
@@ -132,23 +138,22 @@ window.addEventListener("load", function() {
 
     function rowVictory(){
         var domCols = document.getElementsByClassName('col');
-        var domRows = [];
         for (var i = 0; i < boardPositions.length; i++) {
+            var domRows = [];
             var rowToCheck = [];
-            console.log("domCols.children: ", domCols[i].children);
-            for(var j = 0; j < 6; j++) {
-                console.log(boardPositions[i][j]);
-                rowToCheck.push(boardPositions[j][i]);
+            for (var p = 0; p < 7; p++) {
+                var slots = Array.from(domCols[p].children).reverse();
+                domRows.push(slots[i]);
+                rowToCheck.push(boardPositions[p][i]);
             }
+            console.log("rowToCheck: ", rowToCheck);
+            console.log("rowToCheck JOIN: ", rowToCheck.join(""));
             if (rowToCheck.join("").indexOf("rrrr") > -1) {
-                console.log("rowVictory!!! r");
-                addWinningClasses(boardPositions[i], "r", domCols[i].children);
+                addWinningClasses(rowToCheck, "r", domRows.reverse());
                 return true;
             }
             if (rowToCheck.join("").indexOf("yyyy") > -1 ) {
-                console.log("rowVictory!!! y");
-
-                addWinningClasses(boardPositions[i], "y", domCols[i].children);
+                addWinningClasses(rowToCheck, "y", domRows.reverse());
                 return true;
             }
         }
