c6a0aa2f22b4cd82c390494a7b31833d7ecbb358
Peter Anderson <pete.anderson@gmail.com>
2019-12-09 13:59:29 +0100

    Started on victory checks, needs a lot of cleaning up

--- script.js
+++ script.js
@@ -67,22 +67,105 @@ window.addEventListener("load", function() {
 
     console.log(world);
 
+    var boardPositions = [];
+    var position, col, row, gameOver;
 
     function checkPositions() {
+        gameOver = false;
         console.log("*********");
-        for (var i = 0; i < 40; i++) {
-            // console.log("world: ", world.bodies[i].position);
-
+        for (var i = 0; i < 44; i++) {
+            position = world.bodies[i].position;
+            if (position.x > 520 && position.x < 840 && position.y < 520 && position.y > 300) {
+                col = Math.abs(Math.round(525 - position.x)) / 50;
+                row = Math.abs(Math.round((520 - position.y) / 38 - 1));
+                gameOver = updateBoardPosition(col, row, world.bodies[i].render.fillStyle);
+            }
         }
-
         console.log("*********");
-
-        // console.log("world: ", world.bodies[3].position);
-        // console.log("world: ", world.bodies[39].position);
-        // setTimeout(checkPositions, 500);
+        if (gameOver) { return; }
+        setTimeout(checkPositions, 1000);
     }
 
+    generateBoardArray();
     checkPositions();
+
+    function generateBoardArray() {
+        boardPositions = [];
+        for (var k = 0; k < 7; k++) {
+            boardPositions.push(new Array);
+        }
+    }
+
+    function updateBoardPosition(col, row, colour) {
+        if (row !== 0 && !boardPositions[col][row - 1]) { return; }
+        boardPositions[col][row] = colour[0];
+        if (colVictory()) {
+            return true;
+        }
+        if (rowVictory()) {
+            return true;
+        }
+        // console.log("Board Posititons: *****************");
+        // console.log(boardPositions[0]);
+        // console.log(boardPositions[1]);
+        // console.log(boardPositions[2]);
+        // console.log(boardPositions[3]);
+        // console.log(boardPositions[4]);
+        // console.log(boardPositions[5]);
+        // console.log(boardPositions[6]);
+        // console.log("/Board Posititons: *****************");
+    }
+
+    function colVictory(){
+        var domCols = document.getElementsByClassName('col');
+        for (var i = 0; i < boardPositions.length; i++) {
+            if (boardPositions[i].join("").indexOf("rrrr") > -1) {
+                addWinningClasses(boardPositions[i], "r", domCols[i].children);
+                return true;
+            }
+            if (boardPositions[i].join("").indexOf("yyyy") > -1 ) {
+                addWinningClasses(boardPositions[i], "y", domCols[i].children);
+                return true;
+            }
+        }
+    }
+
+    function rowVictory(){
+        var domCols = document.getElementsByClassName('col');
+        var domRows = [];
+        for (var i = 0; i < boardPositions.length; i++) {
+            var rowToCheck = [];
+            console.log("domCols.children: ", domCols[i].children);
+            for(var j = 0; j < 6; j++) {
+                console.log(boardPositions[i][j]);
+                rowToCheck.push(boardPositions[j][i]);
+            }
+            if (rowToCheck.join("").indexOf("rrrr") > -1) {
+                console.log("rowVictory!!! r");
+                addWinningClasses(boardPositions[i], "r", domCols[i].children);
+                return true;
+            }
+            if (rowToCheck.join("").indexOf("yyyy") > -1 ) {
+                console.log("rowVictory!!! y");
+
+                addWinningClasses(boardPositions[i], "y", domCols[i].children);
+                return true;
+            }
+        }
+    }
+
+    function addWinningClasses(col, player, elems) {
+        console.log("col, player", col, player, elems);
+        elems = Array.from(elems).reverse();
+        for (var i = 0; i < col.length; i++) {
+            if (col[i] === player && col[i+1] === player && col[i+2] === player && col[i+3] ===player) {
+                elems[i].children[0].classList.add(player);
+                elems[i + 1].children[0].classList.add(player);
+                elems[i + 2].children[0].classList.add(player);
+                elems[i + 3].children[0].classList.add(player);
+            }
+        }
+    }
 
 
     var wall;
@@ -104,14 +187,18 @@ window.addEventListener("load", function() {
     }
 
     document.body.addEventListener("mouseup", function(e) {
+        gameOver = true;
         var y = mouseConstraint.constraint.pointA.y;
-        console.log(mouseConstraint.constraint.pointA);
         if (y > 520 && y < 530) {
-            console.log(boardBase.isStatic);
             boardBase.isStatic = false;
+            document.getElementsByClassName('board-base')[0].classList.add('open');
             setTimeout(function() {
+                gameOver = false;
+                generateBoardArray();
+                checkPositions();
+                document.getElementsByClassName('board-base')[0].classList.remove('open');
                 boardBase = outerContraints(675, 525, 360, 10);
-            }, 5000);
+            }, 3000);
         }
     });
 
